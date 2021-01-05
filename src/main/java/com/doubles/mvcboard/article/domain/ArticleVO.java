74e589dd19ec7ba67e76895bbc5446a8f9afcd49
sungjae <SungjaeGH.github.io>
2021-01-06 01:55:18 +0900

    기본적인 CRUD 구현 : VO, DAO, Service

---
+++ src/main/java/com/doubles/mvcboard/article/domain/ArticleVO.java
@@ -0,0 +1,14 @@
+package com.doubles.mvcboard.article.domain;
+
+import java.util.Date;
+
+public class ArticleVO {
+    private Integer articleNo;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regDate;
+    private int viewCnt;
+
+    // getter, setter, toString() 생략
+}
